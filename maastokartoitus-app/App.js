6b0552d6b8c98648fb43b728ac7922d6537f6eb4
Markus Lumme <markusjuhani.lumme@gmail.com>
2020-01-16 15:10:38 +0200

    added a initial test for getting location from phone GPS with expo-location

--- maastokartoitus-app/App.js
+++ maastokartoitus-app/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import MapView from 'react-native-maps'
+import * as Location from 'expo-location'
+import * as Permissions from 'expo-permissions'
 
-export default class App extends React.Component {
+export default class App extends Component {
+    state = {
+        location: null,
+        errorMsg: null,
+    }
+
+    getLocation = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+        if(status !== 'granted') {
+            this.setState({ errorMsg: 'Location permission denied' })
+        }
+
+        let location = await Location.getCurrentPositionAsync({})
+        this.setState({ location });
+    }
+
+    componentWillMount() {
+        this.getLocation();
+    }
+
     render() {
         return (
             <View style={styles.container}>
+                <Text>{JSON.stringify(this.state.location)}</Text>
                 <MapView style={styles.mapStyle}/>
             </View>
         );
