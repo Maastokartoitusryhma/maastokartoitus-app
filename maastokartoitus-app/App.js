a957cc1745f1494a6ca7e0ff81bfc6ed32a0fae7
Markus Lumme <markusjuhani.lumme@gmail.com>
2020-01-17 13:57:05 +0200

    Added a map marker on user location, and set map to center initally on user, split map to its own component to enable use of hooks

--- maastokartoitus-app/App.js
+++ maastokartoitus-app/App.js
@@ -1,37 +1,14 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
-import MapView from 'react-native-maps'
-import * as Location from 'expo-location'
-import * as Permissions from 'expo-permissions'
+import MapComponent from './components/MapComponent'
 
 export default class App extends Component {
-    state = {
-        location: null,
-        errorMsg: null,
-    }
-
-    getLocation = async () => {
-        let { status } = await Permissions.askAsync(Permissions.LOCATION);
-
-        if(status !== 'granted') {
-            this.setState({ errorMsg: 'Location permission denied' })
-        }
-
-        let location = await Location.getCurrentPositionAsync({})
-        this.setState({ location });
-    }
-
-    componentWillMount() {
-        this.getLocation();
-    }
-
     render() {
         return (
             <View style={styles.container}>
-                <Text>{JSON.stringify(this.state.location)}</Text>
-                <MapView style={styles.mapStyle}/>
+                <MapComponent style={styles.mapStyle}/>
             </View>
-        );
+        )
     }
 }
 
---
+++ maastokartoitus-app/components/MapComponent.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import MapView, { Marker } from 'react-native-maps'
+import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
+import * as Location from 'expo-location'
+import * as Permissions from 'expo-permissions'
+
+export default MapComponent = ({ style }) => {
+    [ locationState, setLocationState ] = useState({ location: null, region: {latitude: 62.0, longitude: -27.0, latitudeDelta: 1.0, longitudeDelta: 1.0}, errorMsg: null })
+    
+    useEffect(() => {
+        getLocation()    
+    }, [])
+
+    getLocation = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+        if(status !== 'granted') {
+            setLocationState({ location: null, region: null, errorMsg: 'Location permission denied' })
+        }
+
+        let location = await Location.getCurrentPositionAsync({})
+        const region = {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 1.0,
+            longitudeDelta: 1.0,
+        }
+
+        setLocationState({ location, region, errorMsg: null });
+    }
+
+    return (
+        <>
+            <MapView 
+                style = {style}
+                region = {locationState.region}
+            >
+                {locationState.location !== null ? 
+                    <Marker
+                        coordinate = {{
+                            latitude: locationState.location.coords.latitude,
+                            longitude: locationState.location.coords.longitude
+                        }}
+                    />
+                    : null
+                }
+            </MapView>
+        </>
+    )
+}
