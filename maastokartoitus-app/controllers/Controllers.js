eeb73f99fd5a706594f3b147f16facebb77f3375
rpulkka <dreamspace2017@yandex.ru>
2020-01-31 12:31:22 +0200

    App uses GraphQL to fetch the schema from the API.

--- maastokartoitus-app/controllers/Controllers.js
+++ maastokartoitus-app/controllers/Controllers.js
@@ -1,7 +1,30 @@
-import axios from 'axios'
+import ApolloClient, { gql } from 'apollo-boost'
+
+const client = new ApolloClient({
+  uri: 'https://apitest.laji.fi/v0/graphql',
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: 'R7uWGymPsmJ2ItzJphThYBcqLc6dBVDBfdUEGSRYq8aChwRvi34zvfJyrXTTUHFB'
+      }
+    })
+  }
+})
 
 export const getForm = async () => {
-  const response = await axios.get('https://apitest.laji.fi/v0/forms/MHL.1?lang=fi&format=schema&access_token=R7uWGymPsmJ2ItzJphThYBcqLc6dBVDBfdUEGSRYq8aChwRvi34zvfJyrXTTUHFB')
-  console.log(response.data)
-  return null
+  const query = gql`
+    query {
+      form(id: "MHL.1") {
+        id
+        schema
+      }
+    }
+  `
+  try {
+    const response = await client.query({ query })
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    alert(error)
+  }
 }
