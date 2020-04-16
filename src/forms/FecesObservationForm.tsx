0c3e0f11882bd178e1eec224977c2a28ac6a89f9
Hannu Örn <hannu.orn@iki.fi>
2020-04-16 23:55:57 +0300

    Add whitespace

--- src/forms/FecesObservationForm.tsx
+++ src/forms/FecesObservationForm.tsx
@@ -5,14 +5,14 @@ const FecesObservationForm = (
   watch: Function, errors: Object, 
   unregister: Function, defaults: Object | undefined, t: Function) => {
 
-  //variables for the possible default values of the fields
+  // variables for the possible default values of the fields
   let toReturn = []
   let defaultLolifeDroppingsType = null
   let defaultLolifeDroppingsCount = null
   let defaultLolifeDroppingsQuality = null
   let defaultNotes = null
 
-  //hardcoded options for each picker (enum) component
+  // hardcoded options for each picker (enum) component
   const lolifeDroppingsTypeDictionary: { [key: string]: any } = {
     '' : '',
     'MY.lolifeDroppingsTypeRock' : t('MY.lolifeDroppingsTypeRock'),
@@ -33,7 +33,7 @@ const FecesObservationForm = (
     'MY.lolifeDroppingsQuality3' : t('MY.lolifeDroppingsQuality3')
   }
 
-  //inserting the default values into the variables in fields where a default value exists
+  // inserting the default values into the variables in fields where a default value exists
   if (defaults !== undefined) {
     if (defaults.unitFact.lolifeDroppingsType !== undefined) { defaultLolifeDroppingsType = defaults.unitFact.lolifeDroppingsType }
     if (defaults.unitFact.lolifeDroppingsCount !== undefined) { defaultLolifeDroppingsCount = defaults.unitFact.lolifeDroppingsCount }
@@ -41,13 +41,13 @@ const FecesObservationForm = (
     if (defaults.notes !== undefined) { defaultNotes = defaults.notes }
   }
 
-  //inserting the hardcoded default options from the schema to the variables in case where there was no default value from the user
+  // inserting the hardcoded default options from the schema to the variables in case where there was no default value from the user
   if (defaultLolifeDroppingsType === null || defaultLolifeDroppingsType === undefined) { defaultLolifeDroppingsType =  '' }
   if (defaultLolifeDroppingsCount === null || defaultLolifeDroppingsCount === undefined) { defaultLolifeDroppingsCount = '' }
   if (defaultLolifeDroppingsQuality === null || defaultLolifeDroppingsQuality === undefined) { defaultLolifeDroppingsQuality = '' }
-  if(defaultNotes === null || defaultNotes === undefined) {defaultNotes = ''}
+  if (defaultNotes === null || defaultNotes === undefined) {defaultNotes = ''}
 
-  //creating the actual form elements, they're hardcoded and use the above variables to define the default values
+  // creating the actual form elements, they're hardcoded and use the above variables to define the default values
   toReturn.push(createPicker(t('lolifeDroppingsType'), 'lolifeDroppingsType', defaultLolifeDroppingsType, 
     register, setValue, watch, errors, unregister, lolifeDroppingsTypeDictionary))
 
--- src/forms/TrackObservationForm.tsx
+++ src/forms/TrackObservationForm.tsx
@@ -4,12 +4,12 @@ const TrackObservationForm = (register: Function, setValue: Function,
   watch: Function, errors: Object, unregister: Function, defaults: Object | undefined, t: Function
 ) => {
 
-  //variables for the possible default values of the fields
+  // variables for the possible default values of the fields
   let toReturn = []
   let defaultIndirectObservationType = null
   let defaultNotes = null
 
-  //hardcoded options for each picker (enum) component
+  // hardcoded options for each picker (enum) component
   const indirectObservationTypeDictionary: { [key: string]: any } = {
     'MY.indirectObservationTypeSnowTracks' : t('MY.indirectObservationTypeSnowTracks'),
     'MY.indirectObservationTypeUrine' : t('MY.indirectObservationTypeUrine'),
@@ -17,17 +17,17 @@ const TrackObservationForm = (register: Function, setValue: Function,
     'MY.indirectObservationTypeFoodStock' : t('MY.indirectObservationTypeFoodStock')
   }
 
-  //inserting the default values into the variables in fields where a default value exists
-  if(defaults !== undefined) {
-    if(defaults.indirectObservationType !== undefined) { defaultIndirectObservationType = defaults.indirectObservationType }
-    if(defaults.notes !== undefined) { defaultNotes = defaults.notes }
+  // inserting the default values into the variables in fields where a default value exists
+  if (defaults !== undefined) {
+    if (defaults.indirectObservationType !== undefined) { defaultIndirectObservationType = defaults.indirectObservationType }
+    if (defaults.notes !== undefined) { defaultNotes = defaults.notes }
   }
 
-  //inserting the hardcoded default options from the schema to the variables in case where there was no default value from the user
-  if(defaultIndirectObservationType === null) { defaultIndirectObservationType = 'MY.indirectObservationTypeSnowTracks' }
-  if(defaultNotes === null) { defaultNotes = '' }
+  // inserting the hardcoded default options from the schema to the variables in case where there was no default value from the user
+  if (defaultIndirectObservationType === null) { defaultIndirectObservationType = 'MY.indirectObservationTypeSnowTracks' }
+  if (defaultNotes === null) { defaultNotes = '' }
 
-  //creating the actual form elements, they're hardcoded and use the above variables to define the default values
+  // creating the actual form elements, they're hardcoded and use the above variables to define the default values
   toReturn.push(createPicker(t('indirectObservationType'), 'indirectObservationType', defaultIndirectObservationType , register, setValue, watch, errors, unregister, indirectObservationTypeDictionary))
   toReturn.push(createInputElement(t('notes'), 'notes', '', 'string', defaultNotes, register, setValue, watch, errors, unregister, false, undefined))
   return (toReturn)
